b9054aae0a3775249b0db7f7a7be3aa0d91993bc
Redmori <72279911+Redmori@users.noreply.github.com>
2024-12-20 01:33:40 +0100

    Day 19 Part 2

--- src/Day19/Main.java
+++ src/Day19/Main.java
@@ -2,11 +2,8 @@ package Day19;
 
 import AOCutil.AOC;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-import static java.util.Arrays.asList;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,28 +20,14 @@ public class Main {
         System.out.println("Day 19 Part 1: " + p1);
 
 
-//        String testString = "test";
-//        String[]  results = testString.split("test");
-//        for(String str : results){
-//            System.out.println("result: " + str);
-//        }
+        long p2 = countOptionsP2(patterns, splitters);
 
-        //List<List<String>> results = splitOptions("abc||der||fgh||ijk||lmn||opq||rst||uvw||xyz", "||");
-//        List<String> splitters = new ArrayList<>();
-//        splitters.add("||");
-//        splitters.add("##");
-//        if(combineOptions("||####||####||||##||", splitters)){ //abc||der##fgh||ijk||lmn||opq##rst||uvw||xyz
-//            System.out.println("emptied");
-//        }
+        System.out.println("Day 19 Part 2: " + p2);
     }
 
 
-
-
     private static boolean removeFront(String pattern, String[] splitters){
-        //System.out.println(pattern);
         if(pattern.isEmpty()){
-            //System.out.println("emptied");
             return true;
         }
         for(String splitter : splitters){
@@ -62,11 +45,44 @@ public class Main {
     private static int countOptions(List<String> patterns, String[] splitters) {
         int sum = 0;
         for(String pattern : patterns) {
-            //System.out.println("\nnew pattern");
             if (removeFront(pattern, splitters)){
                 sum ++;
             }
         }
+        return sum;
+    }
+
+    private static long removeFrontP2(String pattern, String[] splitters, Map<String, Long> memo) {
+        if (memo.containsKey(pattern)) {
+            return memo.get(pattern);
+        }
+
+        if (pattern.isEmpty()) {
+            return 1;
+        }
+
+        long sum = 0;
+
+        for (String splitter : splitters) {
+            if (pattern.startsWith(splitter)) {
+                String remaining = pattern.substring(splitter.length());
+                sum += removeFrontP2(remaining, splitters, memo);
+            }
+        }
+
+        memo.put(pattern, sum);
+
+        return sum;
+    }
+
+    private static long countOptionsP2(List<String> patterns, String[] splitters) {
+        long sum = 0;
+
+        for (String pattern : patterns) {
+            Map<String, Long> memo = new HashMap<>();
+            sum += removeFrontP2(pattern, splitters, memo);
+        }
+
         return sum;
     }
 
@@ -80,82 +96,5 @@ public class Main {
 
     private static String[] parseSplitters(String s) {
         return s.split(", ");
-    }
-
-
-    public static boolean combineOptions(String option, String[] splitters){
-        if(option.matches("\\.+")){
-            return true;
-        }
-
-        for(String splitter : splitters) {
-            List<String> results = removeOptions(option, splitter);
-            for(String result : results){
-                return combineOptions(result, splitters);
-            }
-        }
-        return false;
-    }
-
-
-    public static List<String> removeOptions(String string, String splitter) {
-        List<String> options = new ArrayList<>();
-
-        int index = 0;
-        index = string.indexOf(splitter, index);
-        while(index != -1) {
-            List<String> combo = new ArrayList<>();
-            String string1 = string.substring(0, index);
-            String string2 = string.substring(index + splitter.length());
-            String newString = string1 + "." + string2;
-            options.add(newString);
-            index += splitter.length();
-            index = string.indexOf(splitter, index);
-            System.out.println(newString);
-        }
-
-        return options;
-
-    }
-
-
-
-
-
-
-    public static boolean splitGatherer(String string, List<String> splitters){
-
-        for(String splitter : splitters){
-            List<List<String>> options = splitOptions(string, splitter);
-            for(List<String> option : options){
-                //TODO check if all parts resolve to empty to return true?
-                for(String part : option){
-                    return splitGatherer(part, splitters);
-                }
-            }
-        }
-        return false;
-    }
-
-
-    public static List<List<String>> splitOptions(String string, String splitter) {
-        List<List<String>> options = new ArrayList<>();
-
-        int index = 0;
-        index = string.indexOf(splitter, index);
-        while(index != -1) {
-            List<String> combo = new ArrayList<>();
-            String string1 = string.substring(0, index);
-            String string2 = string.substring(index + splitter.length());
-            combo.add(string1);
-            combo.add(string2);
-            index += splitter.length();
-            index = string.indexOf(splitter, index);
-            System.out.println(string1 + "<>" + string2);
-            options.add(combo);
-        }
-
-        return options;
-
     }
 }
