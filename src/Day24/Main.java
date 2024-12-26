b016df0c82e06eb5e64c8c408f581ec72a726376
Redmori <rednas92@hotmail.com>
2024-12-26 01:59:57 +0100

    Day 24 part 2

--- src/Day24/Main.java
+++ src/Day24/Main.java
@@ -2,10 +2,7 @@ package Day24;
 
 import AOCutil.AOC;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Objects;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,6 +24,333 @@ public class Main {
         long p1 = convertBinary(binary);
 
         System.out.println("Day 24 Part 1: " + p1);
+
+        ArrayList<Wire> xy = generateXY(input);
+
+        ArrayList<Wire> connections = generateConnections(input, xy);
+
+        ArrayList<Wire> z = getZ(connections, 'z');
+
+//        System.out.println("xy:");
+//        System.out.println(xy);
+//        System.out.println("connections:");
+//        System.out.println(connections);
+//        System.out.println("z:");
+//        System.out.println(z);
+
+        calculateZ(z);
+
+        String zBinary = createBinary(z);
+
+        long p2 = convertBinary(zBinary);
+
+        System.out.println("part 2 base: " + p2);
+
+
+        setDefaults(connections);
+
+        assertInfluence(z);
+
+        for(Wire wire : connections){
+            System.out.println(wire + " " + wire.influence);
+        }
+
+        ArrayList<Wire> x = getZ(xy,'x');
+        ArrayList<Wire> y = getZ(xy,'y');
+        long xValue = convertBinary(createBinary(x));
+        long yValue = convertBinary(createBinary(y));
+        long sum = xValue + yValue;
+        System.out.println(xValue + " + " + yValue + " = " + sum);
+
+        checkXORS(x,y,z, connections);
+
+        countOperations(z);
+
+        displayZ(z, 10);
+
+        //swap("jnt", "vkd", connections);
+        //swap("tgc", "vkd", connections);
+        ArrayList<Wire> adjusted = new ArrayList<>();
+
+        adjusted.addAll(swap("fhc", "z06", connections));
+
+        displayZ(z,15);
+
+        adjusted.addAll(swap("z11", "qhj", connections));
+
+        displayZ(z,25);
+
+        adjusted.addAll(swap("mwh", "ggt", connections));
+
+        displayZ(z,30);
+
+        adjusted.addAll(swap("hqk", "z35", connections));
+
+        displayZ(z,z.size());
+
+
+        countOperations(z);
+
+        calculateZ(adjusted);
+        long xValueAfter = convertBinary(createBinary(x));
+        long yValueAfter = convertBinary(createBinary(y));
+        long sumAfter = xValueAfter + yValueAfter;
+        System.out.println(xValueAfter + " + " + yValueAfter + " = " + sumAfter);
+
+        System.out.println();
+        System.out.println("Day 24 Part 1: " + p1);
+        System.out.println("Day 24 Part 2: fhc,ggt,hqk,mwh,qhj,z06,z11,z35");
+
+    }
+
+    private static void displayZ(ArrayList<Wire> z, int amount){
+        System.out.println("==================");
+        System.out.println(z.get(0));
+
+        System.out.println();
+        System.out.println(z.get(1));
+        System.out.println(z.get(1).wire1 + "      " + z.get(1).wire2);
+
+        for (int i = 2; i < amount; i++) {
+            displaySingleZ(z.get(i));
+        }
+
+        System.out.println("==================");
+    }
+
+    private static void displaySingleZ(Wire z) {
+        System.out.println();
+        System.out.println(z);
+        Wire carryOverWire;
+        if(z.wire1.operation == Wire.Operation.XOR){
+            System.out.println(z.wire1 + "     " + z.wire2);
+            carryOverWire = z.wire2;
+        }else {
+            System.out.println(z.wire2 + "     " + z.wire1);
+            carryOverWire = z.wire1;
+        }
+        if(carryOverWire.wire1.isNearEnd()){
+            System.out.println(carryOverWire.wire1 + "     " + carryOverWire.wire2);
+
+        }else {
+            System.out.println(carryOverWire.wire2 + "     " + carryOverWire.wire1);
+
+        }
+    }
+
+    private static void countOperations(ArrayList<Wire> z){
+        int amount =0;
+        for (int i = 0; i < z.size(); i++) {
+            Wire zWire = z.get(i);
+            int totalOps = zWire.countOperations();
+            int newOps = totalOps - amount;
+            //System.out.println("amount of operations for " + zWire + " is " + newOps);
+            if(!((i == 0 && newOps == 1) || (i == 1 && newOps == 2) || (i == 45 && newOps == 2) || (i > 1 && newOps == 4))){
+                System.out.println("Possible mistake at " + zWire + " because " + newOps + " new operations over expected " );
+            }
+            amount = totalOps;
+        }
+    }
+
+
+    private static void checkXORS(ArrayList<Wire> x, ArrayList<Wire> y, ArrayList<Wire> z, ArrayList<Wire> connections) {
+        System.out.println("Checking XOR gates for the A-wire");
+        for (int i = 1; i < z.size(); i++) {
+            Wire zWire = z.get(i);
+            Wire wire1 = zWire.wire1;
+            Wire wire2 = zWire.wire2;
+
+            Wire aWire;
+            if(wire1.isA(zWire)){
+                aWire = wire1;
+            } else if (wire2.isA(zWire)) {
+                aWire = wire2;
+            } else {
+                System.out.println("ERROR FOUND AT " + zWire);
+                System.out.println("swap options: " + wire1 + " or " + wire2);
+                //find the x0i and y0i
+                for (Wire wire : connections){
+                    if(wire.isEnd()){
+                        continue;
+                    }
+                    if(wire.isAnyA()){
+                        if(wire.wire1.name.contains(String.valueOf(i)) && wire.operation == Wire.Operation.XOR){
+                            System.out.println("with " + wire);
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+    }
+
+    private static String swapAll(ArrayList<Wire> connections, ArrayList<Wire> z) {
+        System.out.println("PERFORMING SWAPPING OPERATION");
+        for (int s11 = 0; s11 < connections.size(); s11++) {
+            for (int s12 = s11 + 1; s12 < connections.size(); s12++) {
+                for (int s21 = s11 + 1; s21 < connections.size(); s21++) {
+                    for (int s22 = s21 + 1; s22 < connections.size(); s22++) {
+                        for (int s31 = s21 + 1; s31 < connections.size(); s31++) {
+                            for (int s32 = s31 + 1; s32 < connections.size(); s32++) {
+                                for (int s41 = s31 + 1; s41 < connections.size(); s41++) {
+                                    for (int s42 = s41 + 1; s42 < connections.size(); s42++) {
+                                        ArrayList<Wire> adjusted = new ArrayList<>();
+                                        adjusted.addAll(connections.get(s11).swap(connections.get(s12)));
+                                        adjusted.addAll(connections.get(s21).swap(connections.get(s22)));
+                                        adjusted.addAll(connections.get(s31).swap(connections.get(s32)));
+                                        adjusted.addAll(connections.get(s41).swap(connections.get(s42)));
+//                                        calculateZ(adjusted);
+//                                        System.out.println(s11 + " " + s21 + " " + s31 + " " + s41 + " gives " + convertBinary(createBinary(z)));
+//                                        System.out.println("count of z adjusted: " + adjusted.size());
+                                    }
+
+                                }
+                                
+                            }
+
+                        }
+                    }
+
+                }
+                
+            }
+        }
+        return "bla";
+    }
+
+    private static ArrayList<Wire> swap(String w1, String w2, ArrayList<Wire> connections) {
+        ArrayList<Wire> adjusted = new ArrayList<>();
+        Wire wire1 = getWireByName(w1, connections);
+        Wire wire2 = getWireByName(w2, connections);
+
+        adjusted.addAll(wire1.swap(wire2));
+
+        return adjusted;
+    }
+
+    private static void assertInfluence(ArrayList<Wire> zs) {
+        for (Wire z : zs){
+            z.assertInfluence();
+        }
+    }
+
+    private static String createBinary(ArrayList<Wire> wires) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = wires.size() - 1; i >= 0; i--){
+            sb.append(wires.get(i).value ? "1" : "0");
+        }
+        return sb.toString();
+    }
+
+    private static void printZ(ArrayList<Wire> zs){
+        for(Wire z : zs){
+            System.out.println(z.toStringWithValue());
+        }
+    }
+
+    private static void printZChained(ArrayList<Wire> zs){
+        for(Wire z : zs){
+            System.out.println(z.toStringChained());
+        }
+    }
+
+    private static void printFull(ArrayList<Wire> wires){
+        for(Wire wire : wires){
+            System.out.println(wire.toStringFull());
+        }
+    }
+
+    private static void calculateZ(ArrayList<Wire> zs) {
+        for(Wire z : zs){
+            z.calculate();
+        }
+    }
+
+    private static ArrayList<Wire> getZ(ArrayList<Wire> wires, char token) {
+        ArrayList<Wire> z = new ArrayList<>();
+        for(Wire wire : wires){
+            if(wire.name.charAt(0) == token){
+                z.add(wire);
+            }
+        }
+
+        z.sort(Comparator.comparing(w -> w.name));
+        return z;
+    }
+
+    private static void setDefaults(ArrayList<Wire> wires) {
+        for(Wire wire : wires){
+            wire.setNormal();
+        }
+    }
+
+    private static void resetToDefaults(ArrayList<Wire> wires) {
+        for(Wire wire : wires){
+            wire.reset();
+        }
+    }
+
+    private static ArrayList<Wire> generateConnections(String[] input, ArrayList<Wire> xy) {
+        int i = 0;
+        while(!Objects.equals(input[i], "")){
+            i++;
+        }
+        i++;
+
+        ArrayList<Wire> connections = new ArrayList<>();
+
+        for(; i < input.length; i++) {
+            String[] split = input[i].split(" ");
+            Wire newWire = getWireByName(split[4], xy, connections);
+
+
+            Wire oldWire1 = getWireByName(split[0], xy, connections);
+            Wire oldWire2 = getWireByName(split[2], xy, connections);
+            //System.out.println("setting wire connections of " + newWire + " with " + oldWire1 + " and " + oldWire2 );
+            newWire.setConnection(oldWire1, split[1], oldWire2); //= new Wire(split[4], oldWire1, split[1], oldWire2);
+            //System.out.println("wiring connection set for: " + newWire.toStringFull());
+        }
+
+        connections.sort(Comparator.comparing(w -> w.name));
+        return connections;
+    }
+
+    private static Wire getWireByName(String name, ArrayList<Wire> wires1, ArrayList<Wire> wires2){
+        Wire foundWire = getWireByName(name, wires1);
+        if(foundWire == null){
+            foundWire = getWireByName(name,wires2);
+        }
+        if(foundWire == null){
+            foundWire = new Wire(name);
+            wires2.add(foundWire);
+        }
+        return foundWire;
+    }
+
+    private static Wire getWireByName(String name, ArrayList<Wire> wires) {
+        for(Wire wire : wires){
+            if(wire.name.equals(name)){
+                return wire;
+            }
+        }
+        return null;
+    }
+
+    private static ArrayList<Wire> generateXY(String[] input) {
+        ArrayList<Wire> xy = new ArrayList<>();
+        for (String line : input) {
+            if (Objects.equals(line, "")) {
+                break;
+            }
+            String[] wireInfo = line.split(": ");
+
+            Wire newWire = new Wire(wireInfo[0], (Objects.equals(wireInfo[1], "1")));
+            xy.add(newWire);
+        }
+        return xy;
     }
 
     private static String createBinaryString(ArrayList<Boolean> binaryArray) {
--- src/Day24/Wire.java
+++ src/Day24/Wire.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Wire {
 
     String name;
+    boolean value;
     boolean normalValue;
 
     boolean isSwapped;
@@ -20,7 +21,68 @@ public class Wire {
 
     Operation operation;
 
-    ArrayList<Wire> influence;
+    ArrayList<Wire> influence = new ArrayList<>();
+
+    public Wire(String nm, boolean v){
+        name = nm;
+        value = v;
+    }
+
+    public Wire(String nm) {
+        name = nm;
+
+    }
+
+    public int countOperations(){
+        if(isEnd()){
+            return 0;
+        }
+        return wire1.countOperations() + wire2.countOperations() + 1;
+    }
+
+    public boolean isA(Wire zWire){
+        String xName = zWire.name.replace('z', 'x');
+        String yName = zWire.name.replace('z', 'y');
+
+        if(isEnd()){
+            return false;
+        }
+
+        if((wire1.name.equals(xName) && wire2.name.equals(yName)) || (wire1.name.equals(yName) && wire2.name.equals(xName))){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean isAnyA(){
+        if(isEnd()){
+            return false;
+        }
+        return wire1.connectedToXY() && wire2.connectedToXY();
+    }
+
+    public boolean connectedToXY(){
+        return name.charAt(0) == 'x' || name.charAt(0) == 'y';
+    }
+
+    public boolean isNearEnd(){
+        if(wire1 == null){
+            return false;
+        }
+        return wire1.isEnd() || wire2.isEnd();
+    }
+
+    public boolean isEnd(){
+        return wire1 == null || wire2 == null;
+    }
+
+
+    public void setConnection(Wire w1, String op, Wire w2) {
+        wire1 = w1;
+        wire2 = w2;
+        operation = stringToOperation(op);
+    }
 
 
     public void assertInfluence(){
@@ -28,9 +90,13 @@ public class Wire {
     }
 
     public void assertInfluence(Wire zWire){
+        if(influence.contains(zWire)) //TODO use set instead?
+            return;
         influence.add(zWire);
-        wire1.assertInfluence(wire1);
-        wire2. assertInfluence(wire2);
+        if(wire1 != null) {
+            wire1.assertInfluence(zWire);
+            wire2.assertInfluence(zWire);
+        }
     }
 
     public ArrayList<Wire> swap(Wire otherWire){
@@ -44,12 +110,85 @@ public class Wire {
         otherWire.wire2 = normalWire2;
         otherWire.operation = normalOperation;
 
-        return influence;
+        ArrayList<Wire> adjusted = new ArrayList<>();
+        adjusted.addAll(influence);
+        adjusted.addAll(otherWire.influence);
+        return adjusted;
+    }
+
+    public void reset(){
+        wire1 = normalWire1;
+        wire2 = normalWire2;
+        operation = normalOperation;
+        value = normalValue;
+    }
+
+    public void setNormal(){
+        normalWire1 = wire1;
+        normalWire2 = wire2;
+        normalOperation = operation;
+        normalValue = value;
+    }
+
+    public boolean calculate(){
+        if(wire1 == null){
+            return value;
+        }
+
+        wire1.calculate();
+        wire2.calculate();
+
+        switch(operation){
+            case Operation.AND:
+                value = wire1.value && wire2.value;
+                break;
+            case Operation.OR:
+                value = wire1.value || wire2.value;
+                break;
+            case Operation.XOR:
+                value = wire1.value ^ wire2.value;
+                break;
+        }
+
+        return value;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append(" - ");
+        if(wire1 != null) {
+            sb.append(wire1.name);
+            sb.append(" ").append(operation).append(" ");
+            sb.append(wire2.name);
+        }else{
+            sb.append(value ? 1 : 0);
+        }
+
+        return sb.toString();
+    }
+
+    public String toStringWithValue(){
+        return name + " - " + (value ? "1" : "0");
+    }
+
+    public String toStringFull(){
+        return name + " " + (value ? "1" : "0") + " - " + wire1.name + " " + (wire1.value ? "1" : "0")  + " " + operation + " " + wire2.name + " " + (wire2.value ? "1" : "0");
+    }
+
+    public String toStringChained(){
+        return name + " " + (value ? "1" : "0") + " (" + (wire1 == null ? "-" : wire1.toStringChained()) + ") " + "("+ (wire2 == null ? "-" : wire2.toStringChained()) + ")";
     }
 
 
-
-
+    public static Operation stringToOperation(String op) {
+        return switch (op) {
+            case "OR" -> Operation.OR;
+            case "AND" -> Operation.AND;
+            case "XOR" -> Operation.XOR;
+            default -> null;
+        };
+    }
 
     enum Operation{
         AND,
